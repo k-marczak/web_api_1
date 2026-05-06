76f2b1462ac57cf28ad50e4e71d6b154fe8e1564
Kamil_hyperbook <k.marczak@savicon.pl>
2026-05-06 13:45:10 +0200

    Added Room and Reservation Model

---
+++ web_api_1/Models/Reservation.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_api_1.Models;
+
+public class Reservation : IValidatableObject
+{
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(1)]
+    public string Topic { get; set; } = string.Empty;
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public string Status { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "EndTime must be later than StartTime.",
+                new[] { nameof(EndTime), nameof(StartTime) }
+            );
+        }
+    }
+}
